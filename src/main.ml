24e6b2645e4123e23dc2a8edac6f0a4a147e106f
Cameron Smith <camrn86@gmail.com>
2012-08-28 11:25:00 -0400

    add other type comments to main

--- src/main.ml
+++ src/main.ml
@@ -2,8 +2,10 @@ open Boolfunc
 open Higherorder
 open Printf
 
+(* - : (bool -> bool -> bool) list = [<fun>; <fun>] *)
 let boolfuncslist = [ Boolfunc.xor; Boolfunc.nand; (&&); (||) ]
 
+(* - : (bool * bool) list = *)
 let boolpairs =
 	[ (false, false); (false, true); (true, false); (true, true) ]
 
@@ -13,6 +15,7 @@ let mapfunslist listin listfunc =
 		(fun f -> List.map (fun boolTup -> f (fst boolTup) (snd boolTup)) listin)
 		listfunc
 
+(* - : bool list list -> unit = <fun> *)
 let _ =
 	let resBool = mapfunslist boolpairs boolfuncslist
 	in List.iter (fun bl -> List.iter (printf "%B\n") bl) resBool
