e631cdc6443a84ddfd6ca691b2e4e20ec760af4f
Cameron Smith <camrn86@gmail.com>
2012-08-28 12:31:34 -0400

    update tests for hobf

--- tests/tests.ml
+++ tests/tests.ml
@@ -1,17 +1,20 @@
-open OUnit;;
-open Calc;;
+open OUnit
+open Boolfunc
+open Higherorder
 
-let test1 () = assert_equal (Calc.add 0 0) 0;;
-let test2 () = assert_equal (Calc.add 1 2) 3;;
+let test1 () = assert_equal (Higherorder.hobf Boolfunc.xor false true) true;;
+let test2 () = assert_equal (Higherorder.hobf Boolfunc.xor true false) true;;
+let test3 () = assert_equal (Higherorder.hobf Boolfunc.nand true true) false;;
 
 (* make sure to add your tests to the suite! *)
 
 let suite =
-  "suite">:::
-    ["test1">:: test1;
-    "test2">:: test2;]
+	"suite" >:::
+	["test1" >:: test1;
+	 "test2" >:: test2;
+	 "test3" >:: test3;]
 ;;
 
 let _ =
-  run_test_tt_main suite
+	run_test_tt_main suite
 ;;
