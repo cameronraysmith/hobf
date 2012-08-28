50027b3637afd4adb5ac3b54ba68c67843eca436
Cameron Smith <camrn86@gmail.com>
2012-08-28 11:09:04 -0400

    operate on list of boolean functions

--- src/main.ml
+++ src/main.ml
@@ -1,27 +1,18 @@
-open Boolfunc;;
-open Higherorder;;
+open Boolfunc
+open Higherorder
+open Printf
 
-print_endline "execution of higher order boolean functions";
-print_newline ();
+let boolfuncslist = [ Boolfunc.xor; Boolfunc.nand; (&&); (||) ]
 
-print_endline "XOR";
-print_endline ("the output of hobf xor 0 0 is: "
-		^ (string_of_bool (Higherorder.hobf Boolfunc.xor false false)));
-print_endline ("the output of hobf xor 0 1 is: "
-		^ (string_of_bool (Higherorder.hobf Boolfunc.xor false true)));
-print_endline ("the output of hobf xor 1 0 is: "
-		^ (string_of_bool (Higherorder.hobf Boolfunc.xor true false)));
-print_endline ("the output of hobf xor 1 1 is: "
-		^ (string_of_bool (Higherorder.hobf Boolfunc.xor true true)));
-print_newline ();
+let boolpairs =
+	[ (false, false); (false, true); (true, false); (true, true) ]
 
-print_endline "NAND";
-print_endline ("the output of hobf nand 0 0 is: "
-		^ (string_of_bool (Higherorder.hobf Boolfunc.nand false false)));
-print_endline ("the output of hobf nand 0 1 is: "
-		^ (string_of_bool (Higherorder.hobf Boolfunc.nand false true)));
-print_endline ("the output of hobf nand 1 0 is: "
-		^ (string_of_bool (Higherorder.hobf Boolfunc.nand true false)));
-print_endline ("the output of hobf nand 1 1 is: "
-		^ (string_of_bool (Higherorder.hobf Boolfunc.nand true true)));
-print_newline ();
+let mapfunslist listin listfunc =
+	List.map
+		(fun f -> List.map (fun boolTup -> f (fst boolTup) (snd boolTup)) listin)
+		listfunc
+
+let _ =
+	let resBool = mapfunslist boolpairs boolfuncslist
+	in List.iter (fun bl -> List.iter (printf "%B\n") bl) resBool
+  
